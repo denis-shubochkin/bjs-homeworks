93d5c92e5a272ee645a1a462bbbef727bd326a40
Denis.Shubochkin <zippo1095@gmail.com>
2020-01-22 21:15:28 +0300

    hw 1.1

---
+++ 1.1-variables/main.js
@@ -0,0 +1,20 @@
+// Задание 1
+let algebra = 5;
+let geography = 5;
+let physics = 5;
+
+let avgBall = (algebra + geography + physics);
+console.log(avgBall);
+
+// Задание 2
+let myName = 'Денис';
+let message = `Привет, мир! Меня зовут ${myName}`;
+console.log(message); 
+
+// Задание 3
+let x = 2;
+let y = 22;
+let z = 0;
+
+let result = (x * y) + (5 * z) + x - 1;
+console.log(result);
