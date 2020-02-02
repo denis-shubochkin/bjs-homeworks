bf9966bcf5455aaab8eefcbd988242f483f9d44b
Denis.Shubochkin <zippo1095@gmail.com>
2020-02-02 19:37:28 +0300

    hw 2.1 done

--- 2.1-wrappers-for-primitives/task.js
+++ 2.1-wrappers-for-primitives/task.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -10,9 +12,43 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+    percent = parseInt(percent);
+    contribution = parseInt(contribution);
+    amount = parseInt(amount);
+    if (isNaN(parseInt(percent)+1))
+    {
+        alert('Проценты должны быть числом');
+        return;
+    }
+    if (isNaN(parseInt(contribution)+1))
+    {
+        alert('Первоначальный взнос должен быть числом');
+        return;
+    }
+    if (isNaN(parseInt(amount)+1))
+    {
+        alert('Сумма кредита должна быть числом');
+        return;
+    }
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+
+    function monthDiff(date, curDate) {
+        let m;
+        m = (date.getFullYear() - curDate.getFullYear()) * 12;
+        return m;
+    }
+    let percentRes = (percent/100)/12;
+    let restAmount = amount - contribution;
+    let dateTo = new Date(date);
+    let now = new Date();
+    let months = monthDiff(dateTo,now);
+    if (months === 0) {
+        alert('Срок кредита не может составлять меньше года');
+        return;
+    }
+    let payMonth = restAmount*(percentRes+percentRes/((Math.pow(1+percentRes,months)-1)));
+    let totalAmount = payMonth*months;
+    return totalAmount.toFixed(2);
 }
 
 function sayHello() {
@@ -23,6 +59,9 @@ function sayHello() {
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+    if (name == "" || name == 'undefined' || name == 'null') {
+        name = 'Аноним';
+    }
+    let greeting = `Привет, мир! Меня зовут ${name}`;
+    return greeting;
 }
